0fd66aaa856ae74750868bcaa69a03eb89230646
jrivera-technisys <jrivera@technisys.com>
2016-12-02 16:27:18 -0300

    added spagueti code from example

---
+++ main.js
@@ -0,0 +1,40 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*(function(){
+       console.log("testing self executing functions");
+       var obj = {
+      init: function(){
+          console.log("testing the init function");
+      } 
+    };
+       obj.init();
+   })() 
+    */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
