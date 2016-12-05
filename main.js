bf0c3b7463572e6fc3a732c9c5bebced27a7fde2
jrivera-technisys <jrivera@technisys.com>
2016-12-05 15:18:41 -0300

    code modify into object literal module, added init and cacheDom functions

--- main.js
+++ main.js
@@ -1,46 +1,40 @@
-var people = [];
-var template = $("#people-template").html();
-
-$("#peopleModule").find("button").on("click",function(){
-    people.push($("#peopleModule").find("input").val());
-    $("peopleModule").find("input").val("");
-    //data for mustache template
-    var data = {
-        people: people,
-    }
-    $("#peopleModule").find("ul").html(Mustache.render(template, data));
-});
-
-$("#peopleModule").find("ul").delegate("i.del", "click", function(e){
-    var $remove = $(e.target).closest("li");
-    var i = $("peopleModule").find("ul").find("li").index($remove);
-    
-    $remove.remove();
-    
-    people.splice(i,1);
-});
-
-/*(function(){
-       console.log("testing self executing functions");
-       var obj = {
-      init: function(){
-          console.log("testing the init function");
-      } 
+(function(){
+var people = {
+        people : [],
+        init: function(){
+            this.cacheDom();
+        },
+        cacheDom: function(){
+            this.$el = $("#peopleModule");
+            this.$button = $el.find("button");
+            this.$input = $el.find("input");
+            this.$ul = $el.find("ul");
+            this.template = this.$el.find("#people-template").html();
+        },
     };
-       obj.init();
-   })() 
-    */
 
+    people.init();  
+})();
 
-
-
-
-
-
-
-
-
-
-
-
-
+//
+//var people = [];
+//var template = $("#people-template").html();
+//
+//$("#peopleModule").find("button").on("click",function(){
+//    people.push($("#peopleModule").find("input").val());
+//    $("peopleModule").find("input").val("");
+//    //data for mustache template
+//    var data = {
+//        people: people,
+//    }
+//    $("#peopleModule").find("ul").html(Mustache.render(template, data));
+//});
+//
+//$("#peopleModule").find("ul").delegate("i.del", "click", function(e){
+//    var $remove = $(e.target).closest("li");
+//    var i = $("peopleModule").find("ul").find("li").index($remove);
+//    
+//    $remove.remove();
+//    
+//    people.splice(i,1);
+//});
